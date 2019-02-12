5a55c741f2041ff3cb3b54c5a54621dca10892d2
torqueShorts <33414164+torqueShorts@users.noreply.github.com>
2019-02-12 21:37:31 +0200

    Add files via upload

---
+++ Hellowworldapp/src/main/java/org/Wyngaard/Chad/App.java
@@ -0,0 +1,23 @@
+package org.Wyngaard.Chad;
+
+import javax.swing.*;
+import java.util.Scanner;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+
+    public static void main( String[] args )
+    {
+
+        String sen;
+
+        sen = JOptionPane.showInputDialog("Hi there my name is xBot5000\nWhat is your name: ");
+
+        JOptionPane.showMessageDialog(null, sen + " is now my new BFF, just kidding, unless you want to be...");
+
+    }
+}
