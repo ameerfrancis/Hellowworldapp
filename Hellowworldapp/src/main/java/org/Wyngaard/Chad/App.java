1524b2c8d96a2925358d4f6ec102cd59d83c9c98
Teagan77 <33413967+Teagan77@users.noreply.github.com>
2019-02-12 22:10:02 +0200

    Update App.java

--- Hellowworldapp/src/main/java/org/Wyngaard/Chad/App.java
+++ Hellowworldapp/src/main/java/org/Wyngaard/Chad/App.java
@@ -3,21 +3,12 @@ package org.Wyngaard.Chad;
 import javax.swing.*;
 import java.util.Scanner;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
-
     public static void main( String[] args )
     {
-
-        String sen;
-
-        sen = JOptionPane.showInputDialog("Hi there my name is xBot5000\nWhat is your name: ");
-
-        JOptionPane.showMessageDialog(null, sen + " is now my new BFF, just kidding, unless you want to be...");
-
+        String fullname;
+        fullname = JOptionPane.showInputDialog("Hey there my name is Teagan\nWhat is your full name? ");
+        JOptionPane.showMessageDialog(null, "Pleased to meet you " + sen + ".");
     }
 }
